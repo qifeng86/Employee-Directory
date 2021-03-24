6556d3e7214db912932da35d19476f7b9f25b505
Qi Feng <qi.feng86@gmail.com>
2021-03-24 17:27:12 -0400

    add app.js and index.js

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "./components/Container";
+import EmployeeList from "./components/EmployeeList";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <EmployeeList />
+    </div>
+  );
+}
+
+export default App;
