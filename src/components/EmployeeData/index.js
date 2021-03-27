b92421b16316827678d31d3e12f30cb07a520df2
Qi Feng <qi.feng86@gmail.com>
2021-03-26 20:53:20 -0400

    add employee data folder

---
+++ src/components/EmployeeData/index.js
@@ -0,0 +1,34 @@
+import React from "react";
+import "./style.css";
+
+
+function EmployeeData(props) {
+    return (
+        <table>
+            <thead>
+                <tr>
+                    <th></th>
+                    <th>First Name</th>
+                    <th onClick={props.sortByLastName}>Last Name</th>
+                    <th>Phone</th>
+                    <th>Email</th>
+                </tr>
+            </thead>
+
+            <tbody>
+                {props.results.map(result => (
+                    <tr>
+                        <td> <img src={result.picture.medium} alt="" /></td>
+                        <td>{result.name.first}</td>
+                        <td>{result.name.last} </td>
+                        <td>{result.phone}</td>
+                        <td>{result.email}</td>
+                    </tr>
+                ))}
+            </tbody>
+        </table>
+    )
+
+}
+
+export default EmployeeData
